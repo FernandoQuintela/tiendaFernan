f89799b1206904cb7e5dd6a6be088a4dbc344213
FernandoQuintela <fernand.quintela@gmail.com>
2022-06-14 01:36:07 -0300

    Commit ItemCount

---
+++ src/components/ItemCount.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+
+function ItemCount({ stock, initial }) {
+
+    const [cant, setCount] = useState(initial);
+
+    function suma() {
+        if (cant >= stock) {alert("Se alcanzó el máximo permitido para este producto")}
+        else 
+        setCount(cant+1);
+    }
+
+    function resta() {
+        if (cant <= 1) {alert("Se alcanzó el mínimo permitido para este producto")}
+        else 
+        setCount(cant-1);
+    }
+
+    return(
+        <div>
+            <button onClick={resta}> - </button>
+            <span> {cant} </span>
+            <button onClick={suma}> + </button>
+            <button>Agregar al Carrito</button>
+        </div>
+    )
+}
+
+export default ItemCount
+
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,8 +1,12 @@
+import React from "react";
+import ItemCount from "./ItemCount";
+
 export default function ItemListContainer(props) {
     console.log(props);
     return (
         <div className="contenedor-items">
             <p>{props.greeting}</p>
+            <ItemCount stock={2} initial={1}></ItemCount>    
         </div>
     )
 }
